f1622199882a39c101a12745906ecf4cbc8ae663
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-12-03 10:25:16 +0700

    #fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -9,14 +9,15 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@contexts': path.resolve(__dirname, './src/context'),
+      "@": path.resolve(__dirname, "src"), // Gốc là 'src'
+      "@components": "@/components", // Không cần lặp lại path.resolve
+      "@services": "@/services",
+      "@contexts": "@/context",
     },
   },
   server: {
-    host: '0.0.0.0', // Đảm bảo Vite lắng nghe trên mọi địa chỉ IP
-    port: 5173, // Cổng mặc định hoặc có thể thay đổi thành một cổng khác nếu cần
+    host: "0.0.0.0", // Lắng nghe trên mọi địa chỉ IP
+    port: 5173, // Đặt cổng mặc định
+    open: true, // Tự động mở trình duyệt khi chạy dev server
   },
 });
