97cabf4c4d9b41102b1501532137744d724b266d
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-12-03 10:29:08 +0700

    #fix: deploy-v2

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,15 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"), // Gốc là 'src'
-      "@components": "@/components", // Không cần lặp lại path.resolve
-      "@services": "@/services",
-      "@contexts": "@/context",
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@contexts": path.resolve(__dirname, "src/context"),
     },
   },
   server: {
-    host: "0.0.0.0", // Lắng nghe trên mọi địa chỉ IP
-    port: 5173, // Đặt cổng mặc định
-    open: true, // Tự động mở trình duyệt khi chạy dev server
+    host: "0.0.0.0",
+    port: 5173,
+    open: true,
   },
 });
